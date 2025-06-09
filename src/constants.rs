e2b62275deed165af8f9ca6a7b168c0f1ba7dd0a
hardfau18 <the.qu1rky.b1t@gmail.com>
2025-06-09 23:24:37 +0530

    feat(serve): serve home and root files with raw option

--- src/constants.rs
+++ src/constants.rs
@@ -10,6 +10,10 @@ pub const SYSTEM_PATH: &'static str = "/view/root";
 /// this path is used to access files from home directory
 pub const HOME_PATH: &'static str = "/view/home";
 
+pub const ARG_RAW: &'static str = "raw";
+
+pub const ARG_RAW_POSSIBLE_VALS: [&'static str; 3] = ["1", "true", "yes"];
+
 pub mod paths {
     use super::*;
     use const_format::concatcp;
@@ -20,6 +24,10 @@ pub mod paths {
     /// path for rendering any file from current workspace
     pub const CURRENT_WORKSPACE_FILE: &'static str =
         concatcp!(CURRENT_WORKSPACE_PATH, "/*file_path");
+    /// load files from root of the file system
+    pub const SYSTEM_FILES: &'static str = concatcp!(SYSTEM_PATH, "/*file_path");
+    /// load files from root of the file system
+    pub const HOME_FILES: &'static str = concatcp!(HOME_PATH, "/*file_path");
 
     pub const DIRECTORY_SERVE: &'static str = "/view/fs";
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![recursion_limit = "512"]
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
     response::{Html, Redirect},
     routing, Router,
 };
@@ -72,21 +72,88 @@ async fn system_files(
     }
 }
 
+async fn read_and_render_file(
+    qparams: HashMap<String, String>,
+    mut file_path: std::path::PathBuf,
+) -> Result<Html<String>, http::StatusCode> {
+    if qparams
+        .get(constants::ARG_RAW)
+        .is_some_and(|val| constants::ARG_RAW_POSSIBLE_VALS.contains(&val.to_lowercase().as_str()))
+    {
+        let content = match tokio::fs::read(&file_path).await {
+            Ok(v) => v,
+            Err(e) => {
+                error!("Couldn't load raw file {file_path:?} : {e}");
+                return Err(http::StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        };
+        match String::from_utf8(content) {
+            Ok(raw_string) => {
+                let page = html::root::Html::builder()
+                    .body(|body_builder| {
+                        body_builder
+                            .division(|div_builder| {
+                                div_builder
+                                    .preformatted_text(|text_builder| text_builder.text(raw_string))
+                            })
+                            .class("raw_div")
+                    })
+                    .build()
+                    .to_string();
+                Ok(Html(page))
+            }
+            Err(e) => {
+                error!("Possible raw file, raw binary files are not yet supported: {e}");
+                Err(http::StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        }
+    } else {
+        // if the extension is not .norg then set it and load the norg file
+        if !file_path.extension().is_some_and(|ext| ext == "norg") {
+            debug!(original_path=?file_path,"setting .norg extension");
+            file_path.set_extension("norg");
+        };
+        debug!(path = %file_path.display(), "Constructed full path for index route");
+        render_norg_file(file_path).await
+    }
+}
+
+#[instrument]
+async fn render_home_file(
+    Query(qparams): Query<HashMap<String, String>>,
+    Path(norg_file_path): Path<std::path::PathBuf>,
+) -> Result<Html<String>, http::StatusCode> {
+    trace!("rendering from system files");
+    let Ok(home_path) = std::env::var("HOME") else {
+        error!("Couldn't get user home directory, this is really odd");
+        return Err(http::StatusCode::INTERNAL_SERVER_ERROR);
+    };
+    let mut file_path = std::path::PathBuf::from(home_path);
+    file_path.push(&norg_file_path);
+    read_and_render_file(qparams, file_path).await
+}
+
+#[instrument]
+async fn render_root_system_file(
+    Path(norg_file_path): Path<std::path::PathBuf>,
+    Query(qparams): Query<HashMap<String, String>>,
+) -> Result<Html<String>, http::StatusCode> {
+    trace!("rendering from system files");
+    let mut file_path = std::path::PathBuf::from("/");
+    file_path.push(&norg_file_path);
+    read_and_render_file(qparams, file_path).await
+}
+
 #[instrument(skip(state))]
 async fn render_current_workspace_file(
     State(state): State<Arc<AppState>>,
+    Query(qparams): Query<HashMap<String, String>>,
     Path(norg_file_path): Path<std::path::PathBuf>,
 ) -> Result<Html<String>, http::StatusCode> {
     trace!("rendering index file");
     let mut file_path = state.root_dir.clone();
     file_path.push(&norg_file_path);
-    // if the extension is not .norg then set it and load the norg file
-    if !file_path.extension().is_some_and(|ext| ext == "norg") {
-        debug!(original_path=?file_path,"setting .norg extension");
-        file_path.set_extension("norg");
-    };
-    debug!(path = %file_path.display(), "Constructed full path for index route");
-    render_norg_file(file_path).await
+    read_and_render_file(qparams, file_path).await
 }
 
 #[derive(Debug, Clone, Subcommand)]
@@ -131,6 +198,11 @@ async fn serve(root_dir: std::path::PathBuf, dev_mode: bool) -> miette::Result<(
             constants::paths::CURRENT_WORKSPACE_FILE,
             routing::get(render_current_workspace_file),
         )
+        .route(constants::paths::HOME_FILES, routing::get(render_home_file))
+        .route(
+            constants::paths::SYSTEM_FILES,
+            routing::get(render_root_system_file),
+        )
         .nest_service("/static", tower_http::services::ServeDir::new("assets"))
         .nest_service(
             constants::paths::DIRECTORY_SERVE,
@@ -162,9 +234,9 @@ async fn serve(root_dir: std::path::PathBuf, dev_mode: bool) -> miette::Result<(
 async fn main() -> miette::Result<()> {
     let args = CmdlineArgs::parse();
     //#[cfg(debug_assertions)]
-    dotenv()
-        .into_diagnostic()
-        .wrap_err("Couldn't load .env file")?;
+    if let Err(e) = dotenv().into_diagnostic() {
+        eprintln!("Couldn't load dotenv file: {e}");
+    }
     #[cfg(debug_assertions)]
     let fmt_event = tracing_subscriber::fmt::format().pretty();
     #[cfg(not(debug_assertions))]
